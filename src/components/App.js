24c6b5399e75cd0bd3d9344b33478d1e20764f21
Sergey <sergey@macbook.local>
2020-08-08 11:37:12 -0700

    init

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function App() {
+
+    /*
+    const task = {
+      id,
+      name,
+      status,
+      priority (number),
+      description
+    }
+    */
+
+
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default App;
