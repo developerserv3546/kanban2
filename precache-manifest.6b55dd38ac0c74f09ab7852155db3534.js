73a5c7ab728e0303e1c263d01631b765c2e2b4ed
Sergey <sergey@macbook.local>
2020-08-11 10:32:05 -0700

    Updates

---
+++ precache-manifest.6b55dd38ac0c74f09ab7852155db3534.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0bce4c09f366f4bfb285e0200198b17",
+    "url": "/kanban2/index.html"
+  },
+  {
+    "revision": "4c411f5f70f8777ffc0c",
+    "url": "/kanban2/static/css/2.047ad264.chunk.css"
+  },
+  {
+    "revision": "d5677b5d78e945432043",
+    "url": "/kanban2/static/css/main.b79788a7.chunk.css"
+  },
+  {
+    "revision": "4c411f5f70f8777ffc0c",
+    "url": "/kanban2/static/js/2.bc5e0419.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/kanban2/static/js/2.bc5e0419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5677b5d78e945432043",
+    "url": "/kanban2/static/js/main.abd3329f.chunk.js"
+  },
+  {
+    "revision": "2396e302952c15b49673",
+    "url": "/kanban2/static/js/runtime-main.6afe77cc.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/kanban2/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/kanban2/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/kanban2/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/kanban2/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/kanban2/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
